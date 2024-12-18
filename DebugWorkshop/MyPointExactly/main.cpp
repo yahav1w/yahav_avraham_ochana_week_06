7c7620bbee68a36af6752faf801394d4c7d35e53
yahav1w <yahavochana111@gmail.com>
2024-12-18 19:45:52 +0200

    class work week 06

---
+++ DebugWorkshop/MyPointExactly/main.cpp
@@ -0,0 +1,17 @@
+#include "Point.h"
+#include <iostream>
+
+
+int main()
+{
+	Point p1(1, 2);
+	Point p2 = p1;
+
+	std::cout << "p1=" << p1 << std::endl;
+	std::cout << "p2=" << p2 << std::endl;
+
+	p2 = p2;
+	std::cout << "p2=" << p2 << std::endl;
+
+	return 0;
+}
